31521ba9220f66c30a8becf7e8f83cdae85a6d2b
jagdish47 <jagdishkumawat81@gmail.com>
2023-12-08 09:35:40 +0530

    Connecting FE to API

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,29 +5,47 @@ import Link from "next/link";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import { TPost } from "../types";
+
+const getPosts = async (email: string) => {
+  try {
+    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/authors/${email}`);
+    const { posts } = await res.json();
+    return posts;
+  } catch (error) {
+    console.error("Error Dashboard Post Fetach", error);
+  }
+};
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
+
+  const email = session?.user?.email;
+  let posts = [];
 
   if (!session) {
     redirect("/sign-in");
   }
 
+  if (email) {
+    posts = await getPosts(email);
+  }
+
   return (
     <div>
-      {postsData && postsData.length > 0 ? (
-        postsData.map((post) => (
+      {posts && posts.length > 0 ? (
+        posts.map((post: TPost) => (
           <Post
             key={post.id}
             id={post.id}
-            author={post.author}
-            authorEmail={"test@gmail.com"}
-            date={post.datepublished}
-            thumbnail={post.thumbnail}
+            author={post.author.name}
+            authorEmail={post.authorEmail}
+            date={post.createdAt}
+            thumbnail={post.imageUrl}
             title={post.title}
             content={post.content}
-            links={post.links || []}
-            category={post.category}
+            links={post.link || []}
+            category={post.catName}
           />
         ))
       ) : (
--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,7 +1,33 @@
-export default function DeleteButton() {
+"use client";
+export default function DeleteButton({ id }: { id: string }) {
+  const handleDelete = async () => {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete this post?"
+    );
+
+    if (confirmed) {
+      try {
+        const res = await fetch(`/api/posts/${id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-type": "application/json",
+          },
+        });
+
+        if (res.ok) {
+          console.log("Post Deleted");
+        }
+      } catch (error) {
+        console.error("Error Delete Post", error);
+      }
+    }
+  };
+
   return (
     <div>
-      <button className="text-red-600">Delete</button>
+      <button onClick={handleDelete} className="text-red-600">
+        Delete
+      </button>
     </div>
   );
 }
